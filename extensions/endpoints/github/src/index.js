39250514923b52fc9937f18e43f00f7f5b1fe6b8
Marcus Schreiter <mail@marcusand.de>
2022-05-14 19:25:33 +0200

    add prettier config

--- extensions/endpoints/github/src/index.js
+++ extensions/endpoints/github/src/index.js
@@ -1,29 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default (router) => {
-	router.post('/*', async (req, res) => {
-		const githubApiUrl = 'https://api.github.com'
-		const { url, headers } = req;
-		const fullUrl = `${githubApiUrl}${url}`;
+  router.post("/*", async (req, res) => {
+    const githubApiUrl = "https://api.github.com";
+    const { url, headers } = req;
+    const fullUrl = `${githubApiUrl}${url}`;
 
-		try {
-			const githubRes = await axios({
-				url: fullUrl,
-				method: 'post',
-				data: {
-					'event_type': 'webhook'
-				},
-				headers: {
-					authorization: headers.authorization,
-					accept: headers.accept
-				}
-			});
+    try {
+      const githubRes = await axios({
+        url: fullUrl,
+        method: "post",
+        data: {
+          event_type: "webhook"
+        },
+        headers: {
+          authorization: headers.authorization,
+          accept: headers.accept
+        }
+      });
 
-			const { status, statusText, data } = githubRes;
-
-			res.send(`${status}: ${statusText} ${data}`)
-		} catch (error) {
-			res.send(`Error: ${error}`)
-		}
-	});
+      const { status, statusText, data } = githubRes;
+      res.send(`${status}: ${statusText} ${data}`);
+    } catch (error) {
+      res.send(`Error: ${error}`);
+    }
+  });
 };
